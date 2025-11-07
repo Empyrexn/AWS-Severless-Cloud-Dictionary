d7c66cfc94748a391c50f91b923ef792865626f8
Jose L. Lopez <lopez3030@mail.fresnostate.edu>
2025-11-07 14:37:52 -0800

    Integrate API Gateway with React frontend

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredTerms, setFilteredTerms] = useState([]);
 
-  const apiUrl = '<ENTER_YOUR_API_URL>'; // Replace with your API Gateway URL
+  const apiUrl = 'https://qs81cnc5yb.execute-api.us-west-2.amazonaws.com/dev';
 
   const handleSearch = () => {
     console.log('Fetching data from API...');
